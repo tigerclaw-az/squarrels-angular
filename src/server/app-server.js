e9d44a5798e9c94e4c40086a43e2b9faeee479fe
Erik Waibel <erik.waibel@gm.com>
2017-07-27 19:59:41 -0700

    Increase size limit for express bodyParser to allow for image data

--- src/server/app-server.js
+++ src/server/app-server.js
@@ -26,8 +26,8 @@ app.set('view engine', 'jade');
 
 app.use(favicon(path.join(__dirname, '../../public/serve', 'favicon.ico')));
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public/serve')));
 app.use(express.static(path.join(__dirname, '../app')));

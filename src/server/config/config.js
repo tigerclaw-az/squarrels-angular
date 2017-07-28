16c8c749a8b79ac6b78b99b36af274decf3e763a
Erik Waibel <erik.waibel@gm.com>
2017-07-28 08:52:45 -0700

    Add getRandom and getRandomStr methods to server/config

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 module.exports = {
 	/* eslint quotes: "off" */
 	apiError: function(err) {
@@ -8,5 +10,19 @@ module.exports = {
 		return {
 			error: err
 		}
-	}
+	},
+
+	getRandom(from, to) {
+		return Math.random() * (to - from) + from;
+	},
+
+	getRandomStr(num) {
+		var self = this;
+
+		return _.times(num, function() {
+			return String.fromCharCode(self.getRandom(96, 122));
+		}).join('').replace(/`/g, ' ');
+	},
+
+	playerImage: 'http://www.fillmurray.com/120/120'
 }

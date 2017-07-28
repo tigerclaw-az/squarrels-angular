56a618fa5c39f1355b8c720e1f17e7f6d4679667
Erik Waibel <erik.waibel@gm.com>
2017-07-28 14:14:48 -0700

    Add additional properties to the PlayerSchema

--- src/server/models/PlayerModel.js
+++ src/server/models/PlayerModel.js
@@ -1,11 +1,26 @@
 var mongoose = require('mongoose'),
 	uuid = require('uuid/v1'),
+	CardsModel = require('./CardsModel'),
 	Schema = mongoose.Schema,
 	PlayerSchema = new Schema({
-		name: String,
-		img: String
+		name: {
+			type: String,
+			trim: true
+		},
+		img: String,
+		isActive: {
+			type: Boolean,
+			default: false
+		},
+		score: {
+			type: Number,
+			default: 0
+		},
+		cardsInHand: [Schema.Types.ObjectId],
+		cardsUsed: [Schema.Types.ObjectId]
 	}, {
-		collection: 'players'
+		collection: 'players',
+		timestamps: true
 	});
 
 module.exports = mongoose.model('Player', PlayerSchema);

f1cfec12c93e0c36b22812981b839ed677e70742
Erik Waibel <erik.waibel@gm.com>
2017-08-01 00:32:35 -0700

    Add more logging information and new 'whoami' case to return private player data

--- src/server/websocket.js
+++ src/server/websocket.js
@@ -1,17 +1,24 @@
-module.exports = function(wss) {
+module.exports = function(server) {
 	var	_ = require('lodash'),
+		uuid = require('uuid/v1'),
+		Player = require('./models/PlayerModel').model,
 		WebSocket = require('ws'),
-		Player = require('./models/PlayerModel');
+		wss = new WebSocket.Server({ port: 1337, server });
 
 	const log = require('loggy');
 
-	wss.broadcast = function broadcast(data, ws, all = true) {
-		log.info(`broadcast(): ${data}`);
-		wss.clients.forEach(function each(client) {
-			log.info(`client: ${client}`);
+	log.info('server:', server);
+	log.info('wss:', wss);
 
+	var conn;
+
+	wss.broadcast = function broadcast(data, all = true) {
+		log.info('broadcast() -> ', data);
+		wss.clients.forEach(function each(client) {
+			log.info('client() -> ', client.readyState);
 			if (client.readyState === WebSocket.OPEN) {
-				if (all || client != ws) {
+				if (all || client != conn) {
+					log.info('send()!');
 					client.send(JSON.stringify(data));
 				}
 			}
@@ -19,31 +26,69 @@ module.exports = function(wss) {
 	};
 
 	wss.on('connection', function connection(ws, req) {
-		log.info(`Connection accepted: ${req.connection.remoteAddress}`);
+		log.info('Connection accepted:', ws);
 		log.info(`Clients Connected: ${wss.clients.size}`);
 
-		wss.broadcast({ action: 'connected', type: 'ws' }, ws);
+		conn = ws;
+
+		ws.send(JSON.stringify({ action: 'connected', type: 'global' }));
 
 		// This is the most important callback for us, we'll handle
 		// all messages from users here.
 		ws.on('message', function(message) {
-			var data = JSON.parse(message);
+			var data = JSON.parse(message),
+				// sessionId = req.session.id,
+				query = {
+					// sessionId
+				};
 
+			log.info('Request:', req.session);
 			// Process WebSocket message
-			log.info(`Message received: ${data}`);
+			log.info('Message received: ', data);
 
 			switch(data.action) {
 				case 'create':
-					wss.broadcast(data, ws, false);
+					wss.broadcast(data, false);
 					break;
+
+				case 'whoami':
+					log.info('websocket:onmessage:whoami -> ', query);
+
+					Player.find(query)
+						.select('+sessionId')
+						.exec()
+						.then(function(list) {
+							if (list.length !== 1) {
+								list = [];
+							}
+
+							let nuts = { action: 'whoami', type: 'players', nuts: list };
+
+							log.info(nuts);
+							ws.send(JSON.stringify(nuts));
+						})
+						.catch(function(err) {
+							log.error(err);
+						});
+					break;
+
 				default:
-					wss.broadcast(data, ws);
+					wss.broadcast(data);
 					break;
 			}
 		});
 
+		ws.on('error', function(err) {
+			log.error(err);
+		});
+
 		ws.on('close', function(connection) {
 			// close user connection
+			log.info('Connection Closed:', connection, wss.clients);
 		});
-	})
-};
+	});
+
+	global.wss = wss;
+
+	return wss;
+}

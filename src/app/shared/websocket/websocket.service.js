a6736a9d9aeb3e28e4f8537ee896b069033b73c0
Erik Waibel <erik.waibel@gm.com>
2017-08-01 00:30:14 -0700

    Convert custom websocket service to use $websocket service as middleman

--- src/app/shared/websocket/websocket.service.js
+++ src/app/shared/websocket/websocket.service.js
@@ -1,33 +1,42 @@
 export class WebSocketService {
-	constructor($rootScope, $log, appConfig, toastr, _) {
+	constructor($rootScope, $log, $websocket, appConfig, toastr, _) {
 		'ngInject';
 
 		this.$log = $log;
 		this.$rootScope = $rootScope;
+		this.$websocket = $websocket;
 
 		this._ = _;
 		this.toastr = toastr;
 
 		this.host = `${appConfig.localhost}:1337`;
 
-		// if user is running mozilla then use it's built-in WebSocket
-		this.WebSocket = (window.WebSocket || window.MozWebSocket);
-
-		this.connection = new this.WebSocket(`ws://${this.host}`);
-
-		this.$log.info('connection', this.connection);
-
-		this.connection.onmessage = this.onMessage.bind(this);
+		this.connect();
 
 		this.$log.info('constructor()', this);
 	}
 
-	send(obj) {
-		var message = typeof obj === 'object' ? JSON.stringify(obj) : obj;
+	connect() {
+		this.$ws = this.$websocket(`ws://${this.host}`);
+
+		this.$log.info('ws', this.$ws);
+
+		this.$ws.onMessage(this.onMessage.bind(this));
+		this.$ws.onError(this.onError.bind(this));
+	}
+
+	send(msg) {
+		var message = typeof msg === 'object' ? JSON.stringify(msg) : msg;
 
 		this.$log.info('send(obj)', message);
 
-		this.connection.send(message);
+		this.$ws.send(msg);
+	}
+
+	onError(msg) {
+		this.$log.info('onError()', msg);
+
+		this.toastr.error('websocket:error', msg);
 	}
 
 	// TODO: Watch for a message coming back from any service using the websocketService
@@ -39,18 +48,20 @@ export class WebSocketService {
 
 		this.$log.info('onMessage()', data, type, action, this);
 
-		if (type === 'player') {
-			switch(action) {
+		if (type === 'players') {
+			switch (action) {
 				case 'create':
+				case 'whoami':
 					data = data.nuts;
 					break;
+
 				default:
 					break;
 			}
 		}
 
-		//--- TESTING ONLY ---//
-		this.toastr.info(JSON.stringify(data), `websocket:${type}:${action}`);
+		// --- TESTING ONLY ---//
+		this.toastr.info(`websocket:${type}:${action}`);
 
 		this.$rootScope.$broadcast('websocket', msg);
 		this.$rootScope.$broadcast(`websocket:${type}:${action}`, data);

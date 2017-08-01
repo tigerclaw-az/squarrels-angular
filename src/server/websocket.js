d59703d243b5f084dfbcdf831dd524c630ca4783
Erik Waibel <erik.waibel@gm.com>
2017-08-01 16:22:27 -0700

    Fix bugs with websocket session id and keep track of all clients in order for calls from API to reference correct client

--- src/server/websocket.js
+++ src/server/websocket.js
@@ -9,61 +9,57 @@ module.exports = function(server, sessionParser, store) {
 
 	let wss = new WebSocket.Server({
 		verifyClient: function(info, done) {
-			logger.log('verifyClient() -> ', info.req.headers);
-			done(info.req.headers);
+			logger.log('verifyClient() -> ', info.req.session, info.req.headers);
+
+			if (info.req.headers.cookie || info.req.session) {
+				done(info.req);
+			}
 		},
 		server
-	});
+	}),
+	CLIENTS = [];
 
-	wss.broadcastAll = function broadcastAll(data) {
-		logger.info('broadcastAll() -> ', data.id);
+	wss.broadcast = function broadcast(data, sid, all = true) {
+		logger.info('broadcast() -> ', data.id);
 		wss.clients.forEach(function each(client) {
 			if (client.readyState === WebSocket.OPEN) {
-				logger.info('send()..');
-				client.send(JSON.stringify(data));
+				if (all || client !== CLIENTS[sid]) {
+					logger.info('send()..');
+					client.send(JSON.stringify(data));
+				}
 			}
 		});
 	};
 
 	wss.on('connection', function connection(ws, req) {
-		let sid = cookieParser.signedCookie(cookie.parse(req.headers.cookie)['connect.sid'], '$eCuRiTy');
+		let parseCookie = cookie.parse(req.headers.cookie)['connect.sid'],
+			sid = cookieParser.signedCookie(parseCookie, '$eCuRiTy');
+
 		logger.info('Connection accepted:', req.headers, req.session);
 		logger.info(`Clients Connected: ${wss.clients.size}`);
 
 		logger.info('sid -> ', sid);
-		store.get(sid, function(err, ss) {
-			logger.log('err -> ', err);
-			logger.log('ss -> ', ss);
-			logger.log(req.sessionID);
-		});
+
+		// Save sessionID against the array of
+		// clients so we can reference later
+		CLIENTS[sid] = ws;
 
 		ws.send(JSON.stringify({ action: 'connected', type: 'global' }));
-
-		wss.broadcast = function broadcast(data) {
-			logger.info('broadcast() -> ', data.action, data.type);
-			wss.clients.forEach(function each(client) {
-				if (client.readyState === WebSocket.OPEN && client !== ws) {
-					logger.info('send()..');
-					client.send(JSON.stringify(data));
-				}
-			});
-		};
 
 		// This is the most important callback for us, we'll handle
 		// all messages from users here.
 		ws.on('message', function(message) {
 			var data = JSON.parse(message),
-				// sessionId = cookieParser(req.headers.cookie, 'cookie.sid'),
 				query = {
-					// sessionId
+					sessionId: sid
 				};
 
 			// Process WebSocket message
 			logger.info('Message received: ', data);
 
-			switch(data.action) {
+			switch (data.action) {
 				case 'whoami':
-					logger.info('websocket:onmessage:whoami -> ', query);
+					logger.info('websocket:onmessage:whoami -> ', query, sid);
 
 					Player.find(query)
 						.select('+sessionId')
@@ -80,7 +76,7 @@ module.exports = function(server, sessionParser, store) {
 					break;
 
 				default:
-					wss.broadcastAll(data);
+					wss.broadcast(data);
 					break;
 			}
 		});
@@ -98,4 +94,4 @@ module.exports = function(server, sessionParser, store) {
 	global.wss = wss;
 
 	return wss;
-}
+};

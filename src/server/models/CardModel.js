28de123e41678a7c7ac40c158e35ed52564761d5
Erik Waibel <erik.waibel@gm.com>
2017-08-02 17:05:36 -0700

    Rename CardsModel to CardModel

---
+++ src/server/models/CardModel.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	CardSchema = new Schema({
+		name: {
+			type: String,
+			required: true
+		},
+		amount: {
+			type: Number,
+			required: true
+		},
+		cardType: {
+			type: String,
+			required: true
+		},
+		action: {
+			type: String
+		}
+	}, {
+		collection: 'cards'
+	});
+
+module.exports = {
+	schema: CardSchema,
+	model: mongoose.model('Card', CardSchema)
+};

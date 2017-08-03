90ac96227520da03d86144a2d2c8fc24fb876184
Erik Waibel <erik.waibel@gm.com>
2017-08-02 17:07:25 -0700

    Add DeckModel to build out mongoose Schema for 'decks'

---
+++ src/server/models/DeckModel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	Card = require('./CardModel'),
+	DeckSchema = new Schema({
+		type: {
+			type: String,
+			required: true
+		},
+		cards: {
+			type: [Schema.Types.ObjectId],
+			ref: Card.model
+		}
+	}, {
+		collection: 'decks',
+		timestamps: true,
+		toObject: {
+			virtuals: true
+		},
+		toJSON: {
+			virtuals: true
+		}
+	});
+
+module.exports = {
+	schema: DeckSchema,
+	model: mongoose.model('Deck', DeckSchema)
+};

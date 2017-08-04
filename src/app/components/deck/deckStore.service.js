d5f2e38e77a136545303a7e3453af6909ad9e0bc
Erik Waibel <erik.waibel@gm.com>
2017-08-04 11:50:45 -0700

    Add logic to deckStore for dealing cards and updating deck

--- src/app/components/deck/deckStore.service.js
+++ src/app/components/deck/deckStore.service.js
@@ -1,5 +1,5 @@
 export default class DeckStoreService {
-	constructor($log, $http, _) {
+	constructor($log, $http, _, decksApi, playersStore) {
 		'ngInject';
 
 		var self = this;
@@ -8,27 +8,56 @@ export default class DeckStoreService {
 		this.$http = $http;
 
 		this._ = _;
+		this.decksApi = decksApi;
+		this.playersStore = playersStore;
 
 		this.model = {
-			cards: {}
+			deck: {}
 		};
 
 		this.$log.info('constructor()', this);
 	}
 
-	getCards(id) {
+	dealCards() {
+		let mainDeck = this._.find(this.model.deck, function(o) {
+			return o.deckType === 'main';
+		});
+
+		this.$log.info('dealCards()', mainDeck, this);
+
+		_.forEach(this.playersStore.model.players, (pl) => {
+			let cards = _.sampleSize(mainDeck.cards, 7);
+
+			this.$log.info('cards:', cards);
+
+			pl.cardsInHand = cards;
+
+			_.pullAll(mainDeck.cards, cards);
+
+			this.$log.info('mainDeck:', this.get(mainDeck.id));
+		});
+
+		// After all cards have been dealt, set the starting player
+		this.playersStore.nextPlayer(-1);
+	}
+
+	get(id) {
 		this.$log.info('get()', id, this);
 
 		if (id) {
-			return this.model.cards[id];
+			return this.model.deck[id];
 		}
 
-		return this.model.cards;
+		return this.model.deck;
 	}
 
-	insert(id, cards) {
-		this.$log.info('insert()', id, cards, this);
+	insert(id, data) {
+		this.$log.info('insert()', id, data, this);
 
-		this.model.cards[id] = cards;
+		this.model.deck[id] = data;
+	}
+
+	update(id, data) {
+		this.model.deck[id] = data;
 	}
 }

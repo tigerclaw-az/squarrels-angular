b5cf45b625817b747ea1ab783ba0c35642019863
Erik Waibel <erik.waibel@gm.com>
2017-08-08 23:53:28 -0700

    Add 'cardType' attribute to CardComponent bindings

--- src/app/components/card/card.component.js
+++ src/app/components/card/card.component.js
@@ -2,8 +2,8 @@ import controller from './card.controller';
 
 export const CardComponent = {
 	bindings: {
-		cardId: '='
-		// type: '@'
+		cardId: '=',
+		cardType: '@'
 	},
 	controller,
 	controllerAs: 'cardCtrl',

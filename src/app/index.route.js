ebf68f8790a473370628a2311b0accbce0b3cb2b
Erik Waibel <erik.waibel@gm.com>
2017-08-10 10:26:16 -0700

    Remove unnecessary 'resolve' code from route

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -11,30 +11,7 @@ export function routerConfig($stateProvider, $urlRouterProvider) {
 			controllerAs: 'mainCtrl',
 			templateUrl: 'main/main.tpl.html',
 			resolve: {
-				// Ensure that we have a connection to the websocket
-				connectWs: ($q, $log, websocket) => {
-					'ngInject';
 
-					let defer = $q.defer(),
-						heartbeats = 0;
-
-					websocket.connect();
-
-					let timer = setInterval(() => {
-						heartbeats++;
-						$log.info('WS STATUS:', websocket.getStatus());
-
-						if (websocket.getStatus() === WebSocket.OPEN) { // Check for 'OPEN'
-							clearInterval(timer);
-							defer.resolve();
-						} else if (heartbeats > 20) {
-							clearInterval(timer);
-							defer.reject();
-						}
-					}, 100);
-
-					return defer;
-				}
 			}
 		})
 		.state('app.home', {

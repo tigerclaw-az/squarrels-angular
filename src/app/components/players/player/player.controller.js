75712aa68d07d2ec483c7319afd30b9746cc4869
Erik Waibel <erik.waibel@gm.com>
2017-08-18 10:43:42 -0700

    Cleanup code for 'storing' cards

--- src/app/components/players/player/player.controller.js
+++ src/app/components/players/player/player.controller.js
@@ -24,69 +24,75 @@ export default class PlayerController {
 		};
 	}
 
+	canStoreCards(cardsSelected) {
+		let cardsMatch = this._.reduce(cardsSelected, (prev, current, index, array) => {
+			let len = array.length,
+				sameCard = prev.amount === current.amount;
+
+			if (!prev) {
+				// If the previous object is 'false', then cards don't match
+				return false;
+			} else if (sameCard && index === len - 1) {
+				// Reached the end of the array and all values matched
+				return true;
+			}
+
+			return sameCard ? current : false;
+		});
+
+		return this.player.isActive &&
+			this._.isEmpty(this.player.actionCard) &&
+			cardsSelected.length === 3 && cardsMatch;
+	}
+
 	getCurrentPlayer() {
 		return this.playerModel.model.player;
 	}
 
 	onStorageClick(evt) {
-		let cardsSelected = this.player.cardsSelected,
-			cardsInStorage = this.player.cardsInStorage,
+		let cardsSelected = this.player.cardsSelected;
+
+		this.$log.debug('onStorageClick()', evt, cardsSelected, this);
+
+		if (this.canStoreCards(cardsSelected)) {
+			this.storeCards(cardsSelected);
+		} else {
+			this.showStorage(this.player);
+		}
+	}
+
+	storeCards(cardsSelected) {
+		let cardsInStorage = this.player.cardsInStorage,
 			cardsInHand = this.player.cardsInHand,
-			// HACK! I tried using _.every(cardsSelected, 'amount') to no avail
-			cardsMatch = this._.reduce(cardsSelected, (prev, current, index, array) => {
-				let len = array.length,
-					sameCard = prev.amount === current.amount;
-
-				this.$log.info('reduce()', prev, current, index, len);
-
-				if (!prev) {
-					// If the previous object is 'false', then cards don't match
-					return false;
-				} else if (sameCard && index === len - 1) {
-					// Reached the end of the array and all values matched
-					return true;
-				}
-
-				return sameCard ? current : false;
-			}),
 			onSuccess = (res => {
 				if (res.status == 200) {
-					this.$log.info(res);
+					this.$log.debug(res);
 				}
 			}),
 			onError = (err => {
 				this.$log.error(err);
 			}),
-			cardIds;
-
-		this.$log.info('onStorageClick()', evt, this);
-		this.$log.info('cardsMatch -> ', cardsSelected, cardsMatch);
-
-		if (this.player.isActive && this._.isEmpty(this.player.actionCard) && cardsSelected.length === 3 && cardsMatch) {
 			cardIds = this._.map(cardsSelected, (obj) => {
 				return obj.id;
 			});
 
-			this.$log.info('Storing your cards: ', cardIds);
+		this.$log.debug('Storing Cards: ', cardIds);
 
-			cardsInStorage.push(cardIds[0]);
-			this._.pullAll(cardsInHand, cardIds);
+		cardsInStorage.push(cardIds[0]);
+		this._.pullAll(cardsInHand, cardIds);
 
-			let plData = {
-				cardsInHand: cardsInHand,
-				cardsInStorage: cardsInStorage,
-				score: this.player.score + cardsSelected[0].amount,
-				totalCards: cardsInHand.length
-			};
+		let plData = {
+			cardsInHand: cardsInHand,
+			cardsInStorage: cardsInStorage,
+			score: this.player.score + cardsSelected[0].amount,
+			totalCards: cardsInHand.length
+		};
 
-			this.playersApi
-				.update(this.player.id, plData)
-				.then(onSuccess, onError);
+		this.playersApi
+			.update(this.player.id, plData)
+			.then(onSuccess, onError);
 
-			this.player.cardsSelected = [];
-		} else {
-			this.showStorage(this.player);
-		}
+		this.player.cardsSelected = [];
 	}
 
 	showStorage(pl) {

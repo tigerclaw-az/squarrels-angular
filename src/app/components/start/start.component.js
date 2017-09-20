f951f8fee0468ff4b2cdf53282a7c949e1d587f7
Erik Waibel <ejwaibel+github@gmail.com>
2017-09-20 12:35:07 -0700

    Put all HTML for startComponent inside code so it can still load if the server connection goes down

--- src/app/components/start/start.component.js
+++ src/app/components/start/start.component.js
@@ -3,5 +3,16 @@ import controller from './start.controller';
 export const StartComponent = {
 	controller,
 	controllerAs: 'startCtrl',
-	templateUrl: 'components/start/start.tpl.html'
+	template: `
+		<div class="winter">
+			<button class="btn btn-primary button-join-game"
+				ng-show="startCtrl.isConnected"
+				ng-click="startCtrl.joinGame()"
+				ng-cloak>JOIN GAME</button>
+
+			<div class="alert alert-danger error" role="alert"
+				ng-show="!startCtrl.isConnected"
+				ng-cloak>Taking a nap. Be back later.</div>
+		</div>
+	`
 };
